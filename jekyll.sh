142beb9b9b1ab30615dfa9e78a05419b93dfb1f9
James Hillyerd <james@hillyerd.com>
2014-05-25 18:48:54 -0700

    Site maintenance
    - Change CNAME to www.inbucket.org to take advanage of github's CDN
    - Switch jekyll to kramdown
    - jekyll.sh deletes _site dir and enables file watching

--- jekyll.sh
+++ jekyll.sh
@@ -3,6 +3,9 @@
 # description: Start ghpages version of jekyll. Opts: -i to install, -u to update
 
 cd "$(dirname $0)"
+if [ -d _site ]; then
+  rm -rf _site
+fi
 
 if [ "$1" == "-i" ]; then
   bundle install
@@ -10,4 +13,4 @@ elif [ "$1" == "-u" ]; then
   bundle update
 fi
   
-bundle exec jekyll serve
+bundle exec jekyll serve --watch
