b3179720a3ac8ecb4afaad298e9a9e710b1a9c9c
James Hillyerd <james@hillyerd.com>
2022-05-29 10:40:42 -0700

    Replace Gem setup with a flake that loads jekyll

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "jekyll environment for inbucket.org";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-utils.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          devShell = pkgs.mkShell {
+            buildInputs = with pkgs; [ jekyll rubyPackages.jekyll-paginate ];
+
+            shellHook = ''
+              alias serve="${pkgs.jekyll}/bin/jekyll serve --watch"
+
+              echo "Commands:"
+              echo "  serve - runs jekyll w/ --watch"
+            '';
+          };
+        }
+      );
+}
