ccad5b831fd425a70653f2708a98915a79011343
James Hillyerd <james@hillyerd.com>
2023-01-16 13:30:08 -0800

    flake: update deps

--- flake.nix
+++ flake.nix
@@ -3,9 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-
     flake-utils.url = "github:numtide/flake-utils";
-    flake-utils.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, nixpkgs, flake-utils }:
