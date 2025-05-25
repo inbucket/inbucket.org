f88b79e86a8b6bd539773ca4a28678dc6ffee481
James Hillyerd <james@hillyerd.com>
2025-05-25 15:33:46 -0700

    Rewrite configurator in gleam+lustre (#53)

--- flake.nix
+++ flake.nix
@@ -6,28 +6,63 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
 
-          serve = pkgs.writeShellScriptBin "serve" ''
-            jekyll serve --watch
+        scripts.serve = pkgs.writeShellScriptBin "serve" ''
+          jekyll serve --watch
+        '';
+
+        # TODO Replace with Jekyll + Lustre GHA
+        scripts.lustre-build = pkgs.writeShellScriptBin "lustre-build" ''
+          set -xeo pipefail
+
+          src=lustre-configurator
+          target=configurator
+
+          cd "$(git rev-parse --show-toplevel)"
+          test -d $src || (echo "ERROR: dir $src does not exist"; exit 5)
+
+          pushd $src
+          gleam clean
+          rm -rf priv
+          gleam run -m lustre/dev build app --minify
+          popd
+
+          test -d $target && rm -rf $target
+          mkdir $target
+          cp -r $src/priv/* $target
+
+          cp $src/index.html $target/index.html
+          sed -i 's|priv/static/configurator\.mjs|configurator/static/configurator.min.mjs|' $target/index.html
+        '';
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            (jekyll.override { withOptionalDependencies = true; })
+            scripts.serve
+            scripts.lustre-build
+
+            # Gleam
+            erlang_27
+            gleam
+            inotify-tools
+            rebar3
+          ];
+
+          shellHook = ''
+            echo
+            echo "Commands:"
+            echo "  serve - runs jekyll w/ --watch"
+            echo "  lustre-build - builds the Lustre configurator"
+            echo
           '';
-        in
-        {
-          devShell = pkgs.mkShell {
-            buildInputs = with pkgs; [
-              (jekyll.override { withOptionalDependencies = true; })
-              serve
-            ];
-
-            shellHook = ''
-              echo "Commands:"
-              echo "  serve - runs jekyll w/ --watch"
-            '';
-          };
-        }
-      );
+        };
+      }
+    );
 }
---
+++ lustre-configurator/README.md
@@ -0,0 +1,9 @@
+# lustre-configurator
+
+The Inbucket Configurator, now in Gleam and Lustre.
+
+## Development
+
+```sh
+gleam run -m lustre/dev start  # Run the dev server
+```
